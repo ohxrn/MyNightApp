7951a402dd24cbf54c884e50172590141408dee7
Jack Ohrn <jackohrn1@gmail.com>
2023-08-14 15:57:29 -0400

    proper location control

--- MyNightApp/Components/HomeUser.js
+++ MyNightApp/Components/HomeUser.js
@@ -1,43 +1,53 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Text } from "react-native";
-import Geolocation from "react-native-geolocation-service";
+import { View, StyleSheet, Text, Button } from "react-native";
+import * as Location from "expo-location";
+
 function HomeUser(props) {
-  const [location, setLocation] = useState(null);
+  const [longitude, setLongitude] = useState();
+  const [latitude, setLatitude] = useState();
+  const [latestLocation, setLatestLocation] = useState(null);
+
+  const getLocation = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) return;
+
+    const options = {
+      accuracy: Location.Accuracy.High,
+      maximumAge: 10,
+      timeout: 1000,
+    };
+
+    try {
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync(options);
+
+      setLatestLocation({ latitude, longitude });
+    } catch (error) {
+      console.log("Error fetching geolocation:", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchGeolocation = async () => {
-      try {
-        const position = await new Promise((resolve, reject) => {
-          Geolocation.getCurrentPosition(resolve, reject, {
-            enableHighAccuracy: true, // Set to false for faster updates
-            timeout: 21000,
-            maximumAge: 0,
-          });
-        });
+    if (latestLocation) {
+      console.log(
+        "UPDATED LOCATION:",
+        `Latitude: ${latestLocation.latitude.toFixed(
+          8
+        )}, Longitude: ${latestLocation.longitude.toFixed(8)}`
+      );
+    }
+  }, [latestLocation]);
 
-        setLocation(position.coords);
-      } catch (error) {
-        console.log("Error fetching geolocation:", error);
-      }
-    };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      getLocation(); // Fetch location on interval
+    }, 10000);
 
-    const intervalId = setInterval(fetchGeolocation, 500); // Fetch every 3 seconds
+    return () => clearInterval(intervalId);
+  }, []); // Empty dependency array to run the effect only once
 
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <Text>Hey</Text>
-      {location && (
-        <Text>
-          Latitude: {location.latitude}, Longitude: {location.longitude}
-        </Text>
-      )}
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
