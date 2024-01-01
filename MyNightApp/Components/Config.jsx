abfddf52eeb81c0d52c2fd33e4b92dc572112348
Jack Ohrn <jackohrn1@gmail.com>
2024-01-01 00:19:16 -0500

    update

--- MyNightApp/Components/Config.jsx
+++ MyNightApp/Components/Config.jsx
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import { getAuth } from "firebase/auth";
+
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -24,7 +24,49 @@ const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
+
 const db = getDatabase(app);
 
-// Export the auth and db objects
+//
+// const uploadToFirebase = async (uri, name) => {
+//   const fetchResponse = await fetch(uri);
+//   const theBlob = await Response.blob();
+//   const storage = getStorage();
+//   const storageRef = ref(storage, "images/rivers.jpg");
+
+//   const uploadTask = uploadBytesResumable(storageRef, file);
+
+//   // Register three observers:
+//   // 1. 'state_changed' observer, called any time the state changes
+//   // 2. Error observer, called on failure
+//   // 3. Completion observer, called on successful completion
+//   uploadTask.on(
+//     "state_changed",
+//     (snapshot) => {
+//       // Observe state change events such as progress, pause, and resume
+//       // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+//       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+//       console.log("Upload is " + progress + "% done");
+//       switch (snapshot.state) {
+//         case "paused":
+//           console.log("Upload is paused");
+//           break;
+//         case "running":
+//           console.log("Upload is running");
+//           break;
+//       }
+//     },
+//     (error) => {
+//       // Handle unsuccessful uploads
+//     },
+//     () => {
+//       // Handle successful uploads on complete
+//       // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+//       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+//         console.log("File available at", downloadURL);
+//       });
+// }
+// );
+// };
+
 export { auth, db };
