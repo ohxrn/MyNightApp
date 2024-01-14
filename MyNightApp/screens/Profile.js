97bf7b070a43dca617e3b2f993286697793e0c26
Jack Ohrn <jackohrn1@gmail.com>
2024-01-13 19:51:25 -0500

    update profile

--- MyNightApp/screens/Profile.js
+++ MyNightApp/screens/Profile.js
@@ -1,11 +1,54 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { auth } from "../Components/Config";
+import { View, Text, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { auth, storage } from "../Components/Config";
+import { getDownloadURL, ref, listAll } from "firebase/storage";
 
 export default function Profile() {
+  const [imageLinks, setImageLinks] = useState();
+  useEffect(() => {
+    const userFolderRef = ref(storage, "PhotoBase/" + auth?.currentUser.uid);
+
+    listAll(userFolderRef)
+      .then((result) => {
+        // result.items is an array of references to each photo
+        const downloadPromises = result.items.map((itemRef) => {
+          return getDownloadURL(itemRef);
+        });
+
+        // Promise.all resolves when all downloadURL promises are resolved
+        return Promise.all(downloadPromises);
+      })
+      .then((downloadURLs) => {
+        // downloadURLs is an array of URLs for each photo
+        // You can handle these URLs as needed, for example, displaying them in your UI
+        console.log(downloadURLs);
+        setImageLinks(downloadURLs);
+      })
+      .catch((error) => {
+        console.error("Error fetching photos:", error);
+      });
+  }, []);
   return (
     <View>
-      <Text>{JSON.stringify(auth)}</Text>
+      <View>
+        <Text>Customize your profile</Text>
+      </View>
+      <View style={{ display: "flex" }}>
+        {imageLinks?.map((url, index) => (
+          <Image
+            key={index}
+            style={{
+              width: 50,
+              height: 50,
+              margin: 5,
+              borderRadius: 49,
+            }}
+            source={{ uri: url }}
+          />
+        ))}
+      </View>
+
+      <View></View>
     </View>
   );
 }
